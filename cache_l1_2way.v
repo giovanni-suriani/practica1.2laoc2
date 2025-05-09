8b9b60957213f630c3149ae91cd5be4dbdeb1734
gixurupita <talcohome@gmail.com>
2025-05-09 01:28:24 -0300

    yes

--- cache_l1_2way.v
+++ cache_l1_2way.v
@@ -13,7 +13,7 @@ module cache_l1_2way (input clk,
 
     // Extracao de partes do endereco
     wire [4:0]  tag    = addr[6:2];   // 5 bits de tag
-    wire        index  = addr[1];   // 1 bit de i­ndice (2 conjuntos)
+    wire        index  = addr[1];   // 1 bit de iÃ‚Â­ndice (2 conjuntos)
     wire        offset = addr[0];  // 1 bit de offset (2 bytes por bloco)
 
     // Estrutura da cache: 2 conjuntos, 2 vias
@@ -22,7 +22,7 @@ module cache_l1_2way (input clk,
     reg        LRU[1:0][1:0];           // [set][way] = Least Recently Used
     reg [16:0] data_array[1:0][1:0];    // [set][way] = dados de 16 bits
 
-    integer i, j;
+    integer i, j, dummy;
     integer way;
     reg [1:0] set_has_invalid_pos;
 
@@ -36,7 +36,7 @@ module cache_l1_2way (input clk,
         begin
             // Verificar se acontece escrita
             if (wren == 1'b1)begin
-                set_has_invalid_pos = set_has_invalid_way();
+                set_has_invalid_pos = set_has_invalid_way(dummy);
                 // Verificar se a posicao eh invalida
                 if (set_has_invalid_pos[0])begin
                     write(way);
@@ -45,7 +45,7 @@ module cache_l1_2way (input clk,
                 end
                 else begin
                     // Atribuimos na memoria na posicao de maior lru
-                    write(biggest_lru_way());
+                    write(biggest_lru_way(dummy));
                     hit  <= 1;
                 end
             end
@@ -53,13 +53,13 @@ module cache_l1_2way (input clk,
             if (wren == 1'b0)begin
                 // Verifica se algum dos dois blocos no conjunto corresponde
                 if (valid_bit[index][0] && tag_array[index][0] == tag) begin
-                    // Se a via 0 é válida e corresponde ao tag
-                    // HIT: verifica se é leitura ou escrita
+                    // Se a via 0 ÃƒÂ© vÃƒÂ¡lida e corresponde ao tag
+                    // HIT: verifica se ÃƒÂ© leitura ou escrita
                     read_hit(0);
                     hit <= 1;
                 end
                 else if (valid_bit[index][1] && tag_array[index][1] == tag) begin
-                    // Se a via 1 é válida e corresponde ao tag
+                    // Se a via 1 ÃƒÂ© vÃƒÂ¡lida e corresponde ao tag
                     read_hit(1);
                     hit <= 1;
                 end
@@ -79,7 +79,7 @@ module cache_l1_2way (input clk,
      */
 
     function [1:0] set_has_invalid_way;
-        input integer dummy;
+        input dummy;
         integer way;
         set_has_invalid_way = 0;
         begin
@@ -106,32 +106,33 @@ module cache_l1_2way (input clk,
     endtask
 
     task write;
-        integer way;
+        input i_way;
         begin
-            atualiza_lru(index, way);                         // Atualiza LRU
-            tag_array[index][way]  <= tag;                    // Atualiza a Tag
-            data_array[index][way] <= data;                   // Escreve na cache
+            atualiza_lru();                         				// Atualiza LRU
+            tag_array[index][i_way]  <= tag;                    // Atualiza a Tag
+            data_array[index][i_way] <= data;                   // Escreve na cache
             q                      <= data;                   // Retorna o dado escrito
         end
     endtask
 
     task read_hit;
-        input way;
-        //if (valid_bit[index][way] && tag_array[index][way] == tag)
+        input i_way;
+        //if (valid_bit[index][i_way] && tag_array[index][i_way] == tag)
         begin
-            atualiza_lru(index, way);
-            tag_array[index][way] <= tag;
-            valid_bit[index][way] <= 1;
+            atualiza_lru();
+            tag_array[index][i_way] <= tag;
+            valid_bit[index][i_way] <= 1;
             q                     <= data_array[index][0];   // Retorna o dado lido
         end
     endtask
 
     task atualiza_lru;
+		  integer i_way;
         begin
-            for (way = 0; way < 2; way = way + 1) begin
-                if (way  != biggest_lru_way(index)) begin
-                    if (valid_bit[index][way])
-                        LRU[index][way] <= LRU[index][way] + 1;
+            for (i_way = 0; i_way < 2; i_way = i_way + 1) begin
+                if (i_way  != biggest_lru_way(index)) begin
+                    if (valid_bit[index][i_way])
+                        LRU[index][i_way] <= LRU[index][i_way] + 1;
                 end
             end
         end
@@ -139,13 +140,13 @@ module cache_l1_2way (input clk,
 
     // importante para logica de quem sai
     function integer biggest_lru_way;
-        input integer index;
-        integer way;
+        input integer dummy;
+        integer i_way;
         begin
             biggest_lru_way = 0;
-            for (way = 0; way < 2; way = way + 1) begin
-                if (LRU[index][way] > LRU[index][biggest_lru_way])
-                    biggest_lru_way = way;
+            for (i_way = 0; i_way < 2; i_way = i_way + 1) begin
+                if (LRU[index][i_way] > LRU[index][biggest_lru_way])
+                    biggest_lru_way = i_way;
             end
         end
     endfunction
